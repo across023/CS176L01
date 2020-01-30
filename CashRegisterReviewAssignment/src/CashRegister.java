899571ebe362cfebbd53129351f9b2152fbecad4
across023 <55259761+across023@users.noreply.github.com>
2020-01-29 20:39:03 -0500

    updates to cashregister

--- CashRegisterReviewAssignment/src/CashRegister.java
+++ CashRegisterReviewAssignment/src/CashRegister.java
@@ -1,4 +1,23 @@
 
-public class CashRegister {
-
+public class CashRegister 
+{
+	private double purchase;
+	private double payment;
+	public CashRegister()
+	{
+		purchase=0;
+		payment=0;
+	}
+	public void recordPurchase(double newPurchase)
+	{
+		purchase+=newPurchase;
+	}
+	public void receivePayment(double newPayment)
+	{
+		payment+=newPayment;
+	}
+	public double giveChange()
+	{
+		return (payment-purchase);
+	}
 }
--- CashRegisterReviewAssignment/src/CashRegisterTester.java
+++ CashRegisterReviewAssignment/src/CashRegisterTester.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
-public class CashRegisterTester {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class CashRegisterTester 
+{
+	public static void main(String[] args) 
+	{
+		CashRegister register = new CashRegister();
+		final double SENTINEL = -1;
+		double itemValue=0;
+		Scanner scan =  new Scanner(System.in);
+		while(itemValue!=SENTINEL)
+		{
+			System.out.println("Enter the value of your item or enter -1 to move on to payment.");
+			itemValue=scan.nextDouble();
+			if(itemValue!=SENTINEL)
+			{
+				register.recordPurchase(itemValue);
+			}
+		}
+		System.out.println("Please enter payment.");
+		register.receivePayment(scan.nextDouble());
+		double change=register.giveChange();
+		System.out.printf("Your change is %.2f",change);
 	}
 
 }
