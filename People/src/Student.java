ec6f278a60b1eff021d6b5abfa65c907f6f05539
across023 <55259761+across023@users.noreply.github.com>
2020-03-05 18:28:23 -0500

    done

--- People/src/Student.java
+++ People/src/Student.java
@@ -1,5 +1,14 @@
 
-public class Student 
+public class Student extends Person
 {
-	
+	private String major;
+	public Student(String newName, int dob, String theirMajor)
+	{
+		super(newName,dob);
+		major=theirMajor;
+	}
+	public String toString()
+	{
+		return super.toString()+", major: "+major;
+	}
 }
